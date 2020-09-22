c2814c3b48ec763f52fd3f0b38b82b9b5e2fb54e
Oscar Eduardo Grajales <69944016+EduardoGHBio@users.noreply.github.com>
2020-09-22 17:28:12 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function do a matrix and transversal matrix
+## his function creates a special "matrix" object 
+##that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inver<-NULL #declarar variable
+  set<- function(y){#dar un nuevo valor a la matriz existente
+    x<<-y
+    inver<<-NULL
+  }
+get<-function()x#Devolver el valor de la matriz existente
+setinver <- function(inverse) inver <<- inverse
+getinver <- function()inver#devuelve el valor de la inversa
+list(set = set, get = get,
+     setinver = setinver,
+     getinver = getinver)
 }
 
-
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" 
+##returned by makeCacheMatrix above.
+##If the inverse has already been calculated 
+##(and the matrix has not changed), 
+##then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inver <- x$getinver()
+  if(!is.null(inver)){
+    message("getting cached data")
+    return(inver)
+  }
+  data <- x$get()
+  inver <- solve(data, ...)
+  x$setinver(inver)
+  inver
 }
+
+##TESTING THE FUNCTIONS
+source("cachematrix.R")
+ensayo<-makeCacheMatrix(matrix(1:20,2,2))
+ensayo$get()
+cacheSolve(ensayo)
